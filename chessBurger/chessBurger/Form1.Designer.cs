57b2ed27aaeafe6cdb35ab0efc5c6031cf986a06
Linicker <olinicker@gmail.com>
2023-11-15 12:25:09 -0300

    adicionando primeira interface

--- chessBurger/chessBurger/Form1.Designer.cs
+++ chessBurger/chessBurger/Form1.Designer.cs
@@ -28,20 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(43, 83, 110);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(932, 76);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(826, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(106, 70);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Consolas", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(219, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(310, 56);
+            label1.TabIndex = 2;
+            label1.Text = "ChessBurger";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(78, 116, 145);
+            panel2.Controls.Add(panel4);
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(216, 493);
+            panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(96, 132, 162);
+            panel4.Location = new Point(-13, -5);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(67, 501);
+            panel4.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(25, 66, 93);
             ClientSize = new Size(932, 492);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             Name = "Form1";
             Text = "ChessBurger";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel4;
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
