f72af2c5639a66e8e4d9d1879977ce06a54dd442
Linicker <olinicker@gmail.com>
2023-11-17 13:39:46 -0300

    procedimento da classe conecta banco

--- chessBurger/chessBurger/ConectaBanco.cs
+++ chessBurger/chessBurger/ConectaBanco.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using MySql.Data.MySqlClient;
 
 
 namespace chessBurger
 {
     internal class ConectaBanco
     {
+        MySqlConnection conexao = new MySqlConnection("server=localhost;user id=root;password=;database=chessburger");
+        string mensagem;
+
+        public bool inserePedido(Pedido novoPedido)
+        {
+            try
+            {
+                conexao.Open();
+                MySqlCommand cmd =
+                    new MySqlCommand("sp_inserePedido", conexao);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("nomeCliente", novoPedido.Nome);
+                cmd.Parameters.AddWithValue("idLanche", novoPedido.Lanche);
+                cmd.ExecuteNonQuery();//executar no banco
+                return true;
+            }
+            catch (MySqlException erro)
+            {
+                mensagem = erro.Message;
+                return false;
+            }
+        }
     }
 }
