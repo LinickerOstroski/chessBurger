555709245af2702b6724662cfbb4742fe2ba4aca
Linicker <olinicker@gmail.com>
2023-11-17 13:22:09 -0300

    criando classe para os pedidos

---
+++ chessBurger/chessBurger/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessBurger
+{
+    class Pedido
+    {
+        string nome;
+        int lanche;
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Lanche { get => lanche; set => lanche = value; }
+    }
+}
